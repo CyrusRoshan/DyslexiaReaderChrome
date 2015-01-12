6d4a9b7133cee12a8d993239e4b73f4329ca0820
CyrusRoshan <CyrusRoshan@users.noreply.github.com>
2015-01-12 00:08:46 -0600

    Added 'off' icon

--- Packaged Folder/js/background.js
+++
@@ -1,2 +0,0 @@
-//rgb(166, 59, 59) background color for status: on
-//rgb(55, 98, 57) background color for status: off
---
+++ Packaged Folder/js/popup.js
@@ -0,0 +1,6 @@
+/*
+rgb(166, 59, 59) background color for status: on
+rgb(55, 98, 57) background color for status: off
+
+
+*/
