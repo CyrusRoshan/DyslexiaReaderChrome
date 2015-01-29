e45310d511e574a1b5347a97a1f9c2378b40e1ad
CyrusRoshan <CyrusRoshan@users.noreply.github.com>
2015-01-29 00:55:55 -0600

    Message Passing fixed. Extension now injects css after DOM is manipulated

--- Packaged Folder/js/background.js
+++ Packaged Folder/js/background.js
@@ -11,6 +11,8 @@ window.lineHeightChecked = true;
 window.ran = false;
 window.timeup = true;
 window.queued = false;
+window.refresh = "200";
+
 //Done>perfect I guess. I'm bad with asynchronous functions.
 //the following functions are basically the same as the functions for popup.html
 //except there wasn't a need to import all of the other scripts if we're only
@@ -83,13 +85,13 @@ function fourth(){
 chrome.runtime.onMessage.addListener(function(message) {
   if (message.modified === true && window.timeup === true) {
       window.timeup = false;
-      setTimeout(function(){window.timeup = true},200);
+      setTimeout(function(){window.timeup = true},window.refresh);
       third();
   }
   else if(window.queued === false) {
     window.queued = true;
-    setTimeout(third(),200);
-    setTimeout(function(){window.queued = true},200);
+    setTimeout(third(),window.refresh);
+    setTimeout(function(){window.queued = true},window.refresh);
   }
 });
 
