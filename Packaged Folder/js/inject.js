24c27319466a09903656ef3aa1228c1342f9a89d
CyrusRoshan <CyrusRoshan@users.noreply.github.com>
2015-02-24 15:48:08 -0600

    still working on a solution that won't require inline styling

--- Packaged Folder/js/inject.js
+++ Packaged Folder/js/inject.js
@@ -1,11 +1,10 @@
-
 function cssInject(){
   checkCss();
   chrome.tabs.executeScript({
     code:
       'var elements = document.getElementsByTagName("*");' +
       'for (var i=0; i < elements.length; i++) {' +
-        '$(elements[i]).addClass( "injectHere" );' +
+        'elements[i].classList.add("' + classes + '");' +
       '}'
   });
   window.ran = true;
@@ -18,7 +17,7 @@ function cssRemove(){
       code:
         'var elements = document.getElementsByTagName("*");' +
         'for (var i=0; i < elements.length; i++) {' +
-          '$(elements[i]).removeClass( "injectHere" );' +
+          'elements[i].classList.remove("injectFfHere", "injectFsHere", "injectBcHere", "injectLhHere");' +
         '}'
     });
   //removes the values from the elements
@@ -31,62 +30,40 @@ function checkCss(){
   window.fontSizeChecked = window.dataStorage["fontSizeChecked"];
   window.backgroundColorChecked = window.dataStorage["backgroundColorChecked"];
   window.lineHeightChecked = window.dataStorage["lineHeightChecked"];
-  window.style = "";
-  chrome.tabs.executeScript({
-    code:
-      'var injectHere = document.getElementsByClassName("injectHere");'
-  });
+  window.classes = "";
+
+  if (fontFamilyChecked === true){
+    classes = "'injectFfHere'";
+  }
+  //get font family ready for injection
+
+  if (fontSizeChecked === true){
+    if (classes != ""){
+      classes += ", 'injectFsHere'";
+    }
+    else {
+      classes = "'injectFsHere'";
+    }
+  }
+  //get font size ready
+
+  if (backgroundColorChecked === true){
+    if (classes != ""){
+      classes += ", 'injectBcHere'";
+    }
+    else {
+      classes = "'injectBcHere'";
+    }
+  }
+  //get background color ready
+
+  if (lineHeightChecked === true){
+    if (classes != ""){
+      classes += ", 'injectLhHere'";
+    }
+    classes = "'injectLhHere', ";
+  }
+  //get line height ready
 
 
-  if (fontFamilyChecked === true) {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.fontFamily = "Courier";'
-    });
-  }
-  else {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.fontFamily = "";'
-    });
-  }
-
-  if (fontSizeChecked === true) {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.fontSize = "18pt";'
-    });
-  }
-  else {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.fontSize = "";'
-    });
-  }
-
-  if (backgroundColorChecked === true) {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.backgroundColor = "#fbfbfb";'
-    });
-  }
-  else {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.backgroundColor = "";'
-    });
-  }
-
-  if (lineHeightChecked === true) {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.lineHeight = "1.5";'
-    });
-  }
-  else {
-    chrome.tabs.executeScript({
-      code:
-        'injectHere.style.lineHeight = "";'
-    });
-  }
 }
